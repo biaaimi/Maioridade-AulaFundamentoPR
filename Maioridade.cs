1da29e1a88f62ddefa8d8aedfcef468538210202
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-06 11:16:05 -0300

    Add files via upload

---
+++ Maioridade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maioriade
+{
+    class Program
+    { 
+static void Main(string[] args)
+{
+    Processar();
+
+    Console.ReadKey();
+
+}
+
+private static void Processar()
+{
+    int Idade;
+    Console.Write("Informa idade: ");
+    int.TryParse(Console.ReadLine(), out Idade);
+
+    if (Idade >= 18)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Acesso Permitido");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Acesso Negado");
+
+    }
+  }
+ }
+}
+
